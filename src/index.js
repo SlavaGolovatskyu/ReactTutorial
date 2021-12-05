0d4c75827d93a286ada7c077092a61d1c8cc987d
SlavaGolovatskyu <slavik.golovatskyu@gmail.com>
2021-12-05 11:58:09 +0200

    testing node-scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import WebFontLoader from 'webfontloader';
-import App from './tutorial-4/App';
+import App from './scss/App';
 // import './css/index.css';
 
 WebFontLoader.load({
---
+++ src/scss/App.js
@@ -0,0 +1,12 @@
+import './index.scss';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="article">
+        <p>Lorem ipsum dolor sit amet consectetur</p>
+        <button>Читать далее...</button>
+      </div>
+    </div>
+  );
+}
